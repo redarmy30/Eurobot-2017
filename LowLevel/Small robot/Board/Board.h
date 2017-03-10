9ce7343e0c1c1b26e21d492dbec08d803a5cf668
LazicBojan <bojan.lazic@skolkovotech.ru>
2017-03-10 12:15:01 +0300

    Added setVoltage and setSpeed for maxon wheels

--- LowLevel/Small robot/Board/Board.h
+++ LowLevel/Small robot/Board/Board.h
@@ -4,7 +4,7 @@
 #include "stdint.h"
 
 char setVoltage(char ch, float duty); // установить напряжение на выходе управления двигателем -1,0 .. 1,0
-
+char setPWM(char ch, float duty);
 extern uint32_t * PWM_CCR[10];  //регистры сравнения каналов ШИМ
 extern uint32_t  PWM_DIR[10];
 extern uint32_t  GENERAL_PIN[10];
